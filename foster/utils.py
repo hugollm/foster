ab14f4c86fca6daab9d67cc9b4c3581d76d5635a
Hugo Mota <hugo.txt@gmail.com>
2017-09-21 23:42:43 -0300

    Fix whitespace in foster/util.py to better comply with PEP8

--- foster/utils.py
+++ foster/utils.py
@@ -6,13 +6,16 @@ from string import Template
 PIKE_DIR = os.path.dirname(__file__)
 SAMPLES_DIR = os.path.join(PIKE_DIR, 'samples')
 
+
 def sample_path(sample):
     path = os.path.join(SAMPLES_DIR, sample)
     return os.path.realpath(path)
 
+
 def copy_sample(sample, target):
     source = os.path.join(SAMPLES_DIR, sample)
     shutil.copy(source, target)
+
 
 def render_sample(sample, **kwargs):
     source = os.path.join(SAMPLES_DIR, sample)
